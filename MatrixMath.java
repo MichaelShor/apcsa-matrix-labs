c234588c1cef468fafbf7c5c24fe266d7d8e6fa3
Michael Shor <mshor24@cgps.org>
2024-02-06 12:31:12 -0500

    first commit - scale method for matrices

--- MatrixMath.java
+++ MatrixMath.java
@@ -2,11 +2,12 @@ public class MatrixMath {
 
     public int determinantTwoByTwo() {
 
+        return 0;
     }
 
     public static int[][] scale(int[][] arr, int scaleFactor) {
 
-        int[][] newArr = new int[0][];
+        int[][] newArr = new int[arr.length][arr[0].length];
         for (int r = 0; r < arr.length; r++) {
             for (int c = 0; c < arr[r].length; c++) {
                 newArr[r][c] = scaleFactor * arr[r][c];
